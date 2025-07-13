9b3bd63c0527f3bda82274277e250c50d10f0e4f
eatmygoose <eat@my.goose>
2025-07-13 08:11:23 +0800

    Added support for image-less QR codes

--- main.py
+++ main.py
@@ -20,20 +20,23 @@ init_state("slider-size", 5)
 payload = st.text_input("QR Payload", placeholder="QR Payload", key="text-payload")
 size = st.slider("Size", min_value=1, max_value=20, key="slider-size")
 redundancy = st.selectbox("Redundancy", options=get_args(udf.QRRedundancy), key="select-redundancy")
-contrast = st.slider("Contrast", min_value=0.0, max_value=4.0, key="slider-contrast")
-brightness = st.slider("Brightness", min_value=0.0, max_value=4.0, key="slider-brightness")
-coloured =st.checkbox("Coloured", key="checkbox-colour")
+
 image = st.file_uploader("Background Image", ["jpg", "png", "bmp", "gif"])
 
-if image:
-    qr_code = generate_qr(
-        payload,
-        size,
-        redundancy,
-        image,
-        coloured,
-        contrast,
-        brightness
-    )
-    st.image(qr_code, caption="QR Preview", use_container_width=True)
-    st.download_button("Download Image", qr_code, file_name=image.name)
+no_image: bool = image is None
+coloured = st.checkbox("Coloured Output", key="checkbox-colour", disabled=no_image)
+contrast = st.slider("Contrast", min_value=0.0, max_value=4.0, key="slider-contrast", disabled=no_image)
+brightness = st.slider("Brightness", min_value=0.0, max_value=4.0, key="slider-brightness", disabled=no_image)
+
+qr_code, qr_code_filename = generate_qr(
+    payload,
+    size,
+    redundancy,
+    image,
+    coloured,
+    contrast,
+    brightness
+)
+st.image(qr_code, caption="QR Preview", use_container_width=True)
+
+st.download_button("Download Image", qr_code, file_name=qr_code_filename)
--- qr_generator.py
+++ qr_generator.py
@@ -3,26 +3,36 @@ import os
 from io import FileIO, BytesIO
 import udf
 from tempfile import TemporaryDirectory
+from typing import Tuple
+from loguru import logger
+from pathlib import Path
+
+def get_filename(path:str) -> str:
+    return Path(path).name
 
 def generate_qr(
     payload: str,
     size: int,
     redundancy: udf.QRRedundancy,
-    image: FileIO,
+    image: FileIO | None,
     color: bool,
     contrast: float,
     brightness: float
-) -> BytesIO:
+) -> Tuple[BytesIO, str]:
     # Shuffle data to-and-fro the filesystem since the library expects files
     with TemporaryDirectory() as temp_dir:
-        # Write uploaded image to temp file 
-        src_file_name:str = os.path.join(temp_dir,image.name)
-        with open(src_file_name, mode="wb+") as input_file_handle:
-            image.seek(0)
-            input_file_handle.write(image.read())
-            input_file_handle.seek(0)
 
-        _, __, output_filename = amzqr.run(
+        src_file_name:str | None = os.path.join(temp_dir,image.name) if image else None
+
+        if src_file_name:
+            logger.info(f"Copying file <{image.name}> to temp dir <{temp_dir}>")
+            # Write uploaded image to temp file 
+            with open(src_file_name, mode="wb+") as input_file_handle:
+                image.seek(0)
+                input_file_handle.write(image.read())
+                input_file_handle.seek(0)
+
+        _, __, output_path = amzqr.run(
             payload,
             version=size,
             level=redundancy,
@@ -33,10 +43,11 @@ def generate_qr(
             save_name=None,
             save_dir=temp_dir
         )
+        logger.info(f"QR Code written to <{output_path}>")
 
-        with open(os.path.join(temp_dir, output_filename), "rb") as output_file:
+        with open(output_path, "rb") as output_file:
             output_file.seek(0)
             copy = BytesIO(output_file.read())
             copy.seek(0)
 
-            return copy
+            return (copy, get_filename(output_path))
